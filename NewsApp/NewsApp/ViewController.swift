118c035367e92dee48ba0302934bedfd7a03d2f2
yusufemirhanarslan <arslanemirhan03.10@gmail.com>
2023-05-14 01:00:02 +0300

    Segmented Control yapıldı.

--- NewsApp/NewsApp/ViewController.swift
+++ NewsApp/NewsApp/ViewController.swift
@@ -10,6 +10,10 @@ import NewsAppAPI
 
 class ViewController: UIViewController {
   
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    
     var viewModel: ViewModelProtocol! {
         didSet {
             viewModel.delegate = self
@@ -19,13 +23,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setSegmentedControl()
+        
        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        viewModel.fetchData()
+        viewModel.fetchData(value: viewModel.setSegmentedControl(index: 0))
+        segmentedControl.selectedSegmentIndex = 0
+    }
+    
+    func setSegmentedControl() {
+        
+        segmentedControl.removeAllSegments()
+        for i in 0...viewModel.segmentedCount - 1 {
+            
+            let name = viewModel.setSegmentedControl(index: i)
+            segmentedControl.insertSegment(withTitle: name, at: i , animated: true)
+           
+        }
+    }
+    
+    
+    @IBAction func seelctedSegmentedControl(_ sender: Any) {
+        
+        let index = segmentedControl.selectedSegmentIndex
+        guard let title = segmentedControl.titleForSegment(at: index) else {return}
+        
+        viewModel.fetchData(value: title)
+        print("Total Value: \(viewModel.segmentedCount)")
     }
     
 }
--- NewsApp/NewsApp/ViewModels/ViewModel.swift
+++ NewsApp/NewsApp/ViewModels/ViewModel.swift
@@ -12,8 +12,10 @@ protocol ViewModelProtocol {
     
     var numberOfItems: Int {get}
     var delegate: ViewModelDelegate? {get set}
+    var segmentedCount: Int {get}
     
-    func fetchData()
+    func fetchData(value: String)
+    func setSegmentedControl(index: Int) -> String
 }
 
 protocol ViewModelDelegate: AnyObject {
@@ -31,30 +33,41 @@ final class ViewModel{
         self.service = service
     }
     
-    fileprivate func fetchNews() {
+    fileprivate func fetchNews(value: String) {
         
-        service.fetchNews(value: "books") { response in
+        service.fetchNews(value: value) { response in
             
             switch response {
                 
             case .success(let news):
                 self.news = news
-                print("News = \(news.results)")
+                print("Value: \(value) \n Variable: \(news)")
             case .failure(let error):
                 print("Error = \(error.localizedDescription)")
             }
         }
     }
     
+    var segmentedControlName = ["arts", "automobiles", "books", "business", "fashion", "food", "health", "home", "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics", "realestate", "science", "sports", "sundayreview", "technology", "theater", "t-magazine", "travel", "upshot", "us", "world"]
+        
+    
 }
-
-
 
 
 extension ViewModel: ViewModelProtocol {
     
-    func fetchData() {
-        fetchNews()
+    var segmentedCount: Int {
+        segmentedControlName.count
+    }
+    
+    
+    func setSegmentedControl(index: Int) -> String {
+        return segmentedControlName[index]
+    }
+    
+    
+    func fetchData(value: String) {
+        fetchNews(value: value)
     }
     
     
